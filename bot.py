5b22e36e146b90db81299086a77f8e44e0dd6860
Obrez2374 <alexskuratov@yandex.ru>
2017-08-22 06:28:04 +0800

    initial commit

--- bot.py
+++ bot.py
@@ -1,27 +1,32 @@
-import requests
 
-url = "https://api.telegram.org/bot<334983253:AAFVY6KOfZGr0cACB1Z-czxDCkkfxjT0KQo>/"
+import requests  
+import datetime
  
+class BotHandler:
  
-def get_updates_json(request):  
-    response = requests.get(request + 'getUpdates')
-    return response.json()
+    def __init__(self, token):
+        self.token = token
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
  
+    def get_updates(self, offset=None, timeout=30):
+        method = 'getUpdates'
+        params = {'timeout': timeout, 'offset': offset}
+        resp = requests.get(self.api_url + method, params)
+        result_json = resp.json()['result']
+        return result_json
  
-def last_update(data):  
-    results = data['result']
-    total_updates = len(results) - 1
-    return results[total_updates]
-	
-def get_chat_id(update):  
-    chat_id = update['message']['chat']['id']
-    return chat_id
+    def send_message(self, chat_id, text):
+        params = {'chat_id': chat_id, 'text': text}
+        method = 'sendMessage'
+        resp = requests.post(self.api_url + method, params)
+        return resp
  
-def send_mess(chat, text):  
-    params = {'chat_id': chat, 'text': text}
-    response = requests.post(url + 'sendMessage', data=params)
-    return response
+    def get_last_update(self):
+        get_result = self.get_updates()
  
-chat_id = get_chat_id(last_update(get_updates_json(url)))
+        if len(get_result) > 0:
+            last_update = get_result[-1]
+        else:
+            last_update = get_result[len(get_result)]
  
-send_mess(chat_id, 'Your message goes here')
+        return last_update
